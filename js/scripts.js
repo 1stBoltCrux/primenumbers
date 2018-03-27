4e10ed12d34f93e38d548c76bc367a6eb86c8f97
Nick Kardish and Ted Leary <ted@>
2018-03-27 14:38:51 -0700

    add loops and arrays to get the prime numbers less than a given number

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
   $("#form1").submit(function(event){
     event.preventDefault();
-    var word = $("#input").val();
-    var wordArray = word.split("");
-    var reverseArray = wordArray.slice().reverse();
-    for(var index = 0; index < wordArray.length; index += 1){
-      if(wordArray[index] !== reverseArray[index]){
-        break;
+    function isPrime(num) {
+      if(num < 2) return false;
+      for (var i = 2; i < num; i++) {
+        if(num%i==0)
+          return false;
+    }
+    return true;
+    }
+
+    var number1 = parseInt($("#input").val());
+    var number2 = 2
+    var array = [];
+    var newArray = [];
+    for (number2; number2 <= number1; number2 += 1) {
+      array.push(number2);
+    }
+    console.log(number2);
+    console.log(array);
+    for(var index = 0; index < number1; index += 1){
+      if(isPrime(array[index]) === true){
+        newArray.push(array[index]);
       }
     }
-    if(index === wordArray.length){
-      alert("This is a palindrome");
-    }
-    else{
-      alert("This is not a palindrome");
-    }
-    console.log(wordArray);
-    console.log(reverseArray);
+    console.log(newArray);
   });
 });
